aa283f0208948373be2838f04f68b8ca6e6c05a5
Valeria <148010378+simlfilh@users.noreply.github.com>
2026-06-02 11:32:52 +0300

    Update requests_for_ZHBU.py

--- PyCharmMiscProject/requests_for_ZHBU.py
+++ PyCharmMiscProject/requests_for_ZHBU.py
@@ -373,8 +373,7 @@ def main():
     # Диалог подтверждения удаления
     if st.session_state.show_delete_confirm:
         with st.container():
-            st.warning(f"⚠️ Вы уверены, что хотите удалить заявку №{st.session_state.delete_id}?
-            Это действие невозможно отменить. Заявка будет полностью удалена из базы данных.")
+            st.warning(f"⚠️ Вы уверены, что хотите удалить заявку №{st.session_state.delete_id}? Это действие невозможно отменить. Заявка будет полностью удалена из базы данных.")
             
             col_yes, col_no = st.columns(2)
             
