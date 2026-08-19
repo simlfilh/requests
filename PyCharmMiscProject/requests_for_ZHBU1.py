51a968074b89a596c4cfac70e9e1ca83962cf817
Valeria <148010378+simlfilh@users.noreply.github.com>
2026-08-19 15:26:12 +0300

    Update requests_for_ZHBU1.py

--- PyCharmMiscProject/requests_for_ZHBU1.py
+++ PyCharmMiscProject/requests_for_ZHBU1.py
@@ -423,9 +423,9 @@ def to_excel(df, dormitory=None, request_type=None):
     if columns_to_drop:
         df_to_export = df_to_export.drop(columns=columns_to_drop)
     
-    # Для общежития №2 и типа "Сантехника" удаляем столбец "Комната" (опционально)
+    # Для общежития №2 и №7 с типом "Сантехника" удаляем столбец "Комната"
     if dormitory and request_type:
-        if "Общежитие №2" in dormitory and "Сантехника" in request_type:
+        if ("Общежитие №2" in dormitory or "Общежитие №7" in dormitory) and "Сантехника" in request_type:
             if 'Комната' in df_to_export.columns:
                 df_to_export = df_to_export.drop(columns=['Комната'])
     
